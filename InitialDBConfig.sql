5d746f119a17b9eaa0f421386a5bcd68b828855f
Sidorenko Andrey <sidoran@gmail.com>
2019-10-17 17:31:02 +0300

    Add initial data

---
+++ InitialDBConfig.sql
@@ -0,0 +1,19 @@
+﻿DELETE FROM  Agents
+DBCC CHECKIDENT (Agents, RESEED, 0 )
+DELETE FROM [Events]
+DBCC CHECKIDENT ([Events], RESEED, 0 )
+DELETE FROM RunBooks
+DBCC CHECKIDENT (RunBooks, RESEED, 0 )
+DELETE FROM RunTasks
+DBCC CHECKIDENT (RunTasks, RESEED, 0 )
+
+
+INSERT INTO Agents ([Name], [IpAddress])
+VALUES('IH-DC2-Bot01','172.22.20.111'),
+	  ('IH-DC2-Bot02','172.22.20.112')
+
+INSERT INTO RunBooks([Name],[Author],[isActive],[WhenChanged],[WhenCreated])
+VALUES ('Test Runbook','Sidorenko Andrey',1, GETDATE(),GETDATE())
+
+INSERT INTO RunTasks ([Name],[Author],[RunBookId],[RunbookOrder],[IsActive], [WhenChanged], [WhenCreated])
+VALUES ('Hello World','Sidorenko Andrey',1, 1, 1,GETDATE(),GETDATE())
